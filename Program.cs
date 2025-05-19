702930eed901a673986203178e4c84162092813d
SaikatBaidya <sbai858@aucklanduni.ac.nz>
2025-05-19 23:29:47 +1200

    First commit: runnable server as a starting point

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketTest
+{
+    public class SynchronousSocketListener
+    {
+        public void StartListening()
+        {
+            const int BACKLOG = 10;
+            const int DEFPORTNUM = 11000;
+
+            // Use loopback IP (localhost)
+            IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, DEFPORTNUM);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(BACKLOG);
+
+                while (true)
+                {
+                    Console.WriteLine($"Waiting for a connection at {localEndPoint.Port}...");
+
+                    Socket handler = listener.Accept();
+                    var remoteEndPoint = (IPEndPoint)handler.RemoteEndPoint!;
+                    byte[] bytes = new byte[1024];
+                    string data = string.Empty;
+
+                    int bytesRec = 0;
+                    while (true)
+                    {
+                        bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf('\n') > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine($"Received text from {remoteEndPoint}: {data}");
+
+                    byte[] msg = Encoding.ASCII.GetBytes($"[{data}]");
+                    handler.Send(msg);
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.ReadLine();
+        }
+
+        public static int Main(string[] args)
+        {
+            new SynchronousSocketListener().StartListening();
+            return 0;
+        }
+    }
+}
