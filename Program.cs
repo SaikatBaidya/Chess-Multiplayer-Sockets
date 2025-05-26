035995af8f2e25ca97c1144e167c997862696575
SaikatBaidya <sbai858@aucklanduni.ac.nz>
2025-05-27 05:50:53 +1200

    Implement /theirmove endpoint to allow players to retrieve the opponent's last move

--- Program.cs
+++ Program.cs
@@ -262,6 +262,88 @@ namespace SocketTest
                                     }
                                 }
                             }
+                            else if (requestLine[1].StartsWith("/theirmove"))
+                            {
+                                // Manual query string parsing for player and id
+                                string player = null, gameId = null;
+                                string url = requestLine[1];
+                                int qIndex = url.IndexOf('?');
+                                if (qIndex != -1 && url.Length > qIndex + 1)
+                                {
+                                    string query = url.Substring(qIndex + 1);
+                                    var pairs = query.Split('&');
+                                    foreach (var pair in pairs)
+                                    {
+                                        var kv = pair.Split('=');
+                                        if (kv.Length == 2)
+                                        {
+                                            if (kv[0] == "player") player = Uri.UnescapeDataString(kv[1]);
+                                            else if (kv[0] == "id") gameId = Uri.UnescapeDataString(kv[1]);
+                                        }
+                                    }
+                                }
+
+                                if (string.IsNullOrEmpty(player) || string.IsNullOrEmpty(gameId))
+                                {
+                                    string responseBody = "Missing parameter";
+                                    string response =
+                                        "HTTP/1.1 400 Bad Request\r\n" +
+                                        "Content-Type: text/plain\r\n" +
+                                        $"Content-Length: {Encoding.UTF8.GetByteCount(responseBody)}\r\n" +
+                                        "Connection: keep-alive\r\n" +
+                                        "\r\n" +
+                                        responseBody;
+                                    handler.Send(Encoding.UTF8.GetBytes(response));
+                                }
+                                else
+                                {
+                                    string opponentMove = null;
+                                    bool found = false;
+                                    lock (_lock)
+                                    {
+                                        foreach (var game in activeGames)
+                                        {
+                                            if (game.GameId == gameId)
+                                            {
+                                                if (game.Player1 == player)
+                                                    opponentMove = game.LastMove2;
+                                                else if (game.Player2 == player)
+                                                    opponentMove = game.LastMove1;
+                                                else
+                                                    break; // player not in this game
+
+                                                found = true;
+                                                break;
+                                            }
+                                        }
+                                    }
+
+                                    if (found)
+                                    {
+                                        string responseBody = $"{{\"move\":{(opponentMove == null ? "null" : $"\"{opponentMove}\"")}}}";
+                                        string response =
+                                            "HTTP/1.1 200 OK\r\n" +
+                                            "Content-Type: application/json\r\n" +
+                                            $"Content-Length: {Encoding.UTF8.GetByteCount(responseBody)}\r\n" +
+                                            "Connection: keep-alive\r\n" +
+                                            "\r\n" +
+                                            responseBody;
+                                        handler.Send(Encoding.UTF8.GetBytes(response));
+                                    }
+                                    else
+                                    {
+                                        string responseBody = "Game or player not found";
+                                        string response =
+                                            "HTTP/1.1 404 Not Found\r\n" +
+                                            "Content-Type: text/plain\r\n" +
+                                            $"Content-Length: {Encoding.UTF8.GetByteCount(responseBody)}\r\n" +
+                                            "Connection: keep-alive\r\n" +
+                                            "\r\n" +
+                                            responseBody;
+                                        handler.Send(Encoding.UTF8.GetBytes(response));
+                                    }
+                                }
+                            }
                             else
                             {
                                 // 404 Not Found for other endpoints
--- obj/Debug/net8.0/GameServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be6a25d52d194bd6a64e2bca4a6f7b755f15f39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7fa16a04f5fd433b6fb0a3d8b4b783aadc1de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
